5ed281b27b7b9bb7816fb89f39c86596e2bf0b1c
Vianney Marticou <vianney.marticou@epita.fr>
2022-10-27 15:34:25 +0200

    [TOOLS] Create function generate_email

--- anti-scam-toolkit.py
+++ anti-scam-toolkit.py
@@ -5,9 +5,17 @@ def get_firstname(filename="names.txt") :
     myline = rd.choice(lines)
     return myline
 
+def get_extension(filename="extension.txt") :
+    lines = open(filename).read().splitlines()
+    myline = rd.choice(lines)
+    return myline
+
+def generate_email(firstname=get_firstname(),lastname=get_firstname(),extension=get_extension()) :
+    return firstname+"."+lastname+"@"+extension
 
 def __test() :
     print("firstname :",get_firstname())
+    print("email :",generate_email())
 
 if __name__=="__main__" :
     __test()
