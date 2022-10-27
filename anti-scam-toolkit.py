223193c02b3a855dcf7b54d169e6659e4bd018dd
Vianney Marticou <vianney.marticou@epita.fr>
2022-10-27 15:42:07 +0200

    [TOOLS] Create the function generate_number

--- anti-scam-toolkit.py
+++ anti-scam-toolkit.py
@@ -13,9 +13,20 @@ def get_extension(filename="extension.txt") :
 def generate_email(firstname=get_firstname(),lastname=get_firstname(),extension=get_extension()) :
     return firstname+"."+lastname+"@"+extension
 
+def generate_number(country="") :
+    result = country
+    if country == "" :
+        result = "0"
+    for i in range(9) :
+        result+= str(rd.randint(0,9))
+    return result
+
 def __test() :
     print("firstname :",get_firstname())
     print("email :",generate_email())
+    for i in range(4) :
+        print("number :",generate_number())
+    print("number :", generate_number("+33"))
 
 if __name__=="__main__" :
     __test()
